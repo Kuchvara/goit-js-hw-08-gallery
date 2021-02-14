a7e271084a1e036ffb404c432ca8a0f63dc6c497
Ivan Kuchvara <ivan.kuchvara@gmail.com>
2021-02-14 21:09:36 +0200

    some fixes
    реалізовано мінімум для здачі дз

--- js/realisation.js
+++ js/realisation.js
@@ -8,26 +8,54 @@ import images from './gallery-items.js';
 
 const refs = {
     gallery: document.querySelector('ul'),
-    modal: document.querySelector('div.lightbox'),
+    modal: document.querySelector('div.js-lightbox'),
     overlay: document.querySelector('div.lightbox__overlay'),
     modalContent: document.querySelector('img.lightbox__image'),
     modalCloseBtn: document.querySelector('button'),
 }
-
+console.log(refs.modalContent)
+// створюємо розмітку галереї (динамічно)
 const createGallery = images.map(item => {
     const liRef = document.createElement('li');
     liRef.classList.add('gallery__item');
+
     const linkRef = document.createElement('a');
     linkRef.classList.add('gallery__link');
-    linkRef.setAttribute('href', item.original);
+    linkRef.href = item.original;
+
     const imgRef = document.createElement('img');
     imgRef.classList.add('gallery__image');
-    imgRef.setAttribute('src', item.preview);
-    imgRef.setAttribute('data-source', item.original);
-    imgRef.setAttribute('alt', item.description);
-    return (liRef, linkRef, imgRef)
+    imgRef.src = item.preview;
+    imgRef.dataset.source = item.original;
+    imgRef.alt = item.description;
+
+    linkRef.append(imgRef);
+    liRef.append(linkRef);
+    return liRef
 });
 
 refs.gallery.append(...createGallery);
 
+// робимо колбек функції для кліків
 
+function clickOnGallery(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') { return };
+    
+    refs.modalContent.src = event.target.dataset.source;
+    refs.modalContent.alt = event.target.alt;
+
+    refs.modal.classList.add('is-open');
+}
+
+function closeModal(event) {
+    refs.modalContent.src = '';
+    refs.modalContent.alt = '';
+
+    refs.modal.classList.remove('is-open');
+}
+
+// додаєм EventListener 
+refs.gallery.addEventListener('click', clickOnGallery);
+refs.modalCloseBtn.addEventListener('click', closeModal);
+refs.overlay.addEventListener('click', closeModal);
